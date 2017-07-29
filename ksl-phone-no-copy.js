f2990af3cfe47ec0595671fbdec2c952c307fed5
thelaurence <thelaurence@users.noreply.github.com>
2017-07-29 16:03:43 -0600

    Create ksl-phone-no-copy.js

---
+++ ksl-phone-no-copy.js
@@ -0,0 +1,4 @@
+"javascript: var phonenumber = document.createElement('phone-number');
+phonenumber.textContent = document.getElementsByClassName('listingContactSeller-homePhone')[0].childNodes[1].innerText;
+document.appendChild(phonenumber);
+phonenumber.select(); document.execCommand('copy');"
